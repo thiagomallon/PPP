ed3eebcfd00d0a54c849a75c6c871c3b18aa3030
Thiago Mallon <thiagomallon@gmail.com>
2015-05-19 02:10:46 -0300

    Updating structure

---
+++ src/Model/User.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by Thiago Mallon
+ */
+
+/**
+ * @package App\Model
+ */
+namespace App\Model;
+
+/**
+ * Classe User. Model de usuários
+ * @author Thiago Mallon <thiagomallon@gmail.com>
+ */
+class User
+{
+    /**
+     * Property stores user id
+     * @var int $id
+     */
+    protected $id;
+    
+    /**
+     * Property stores username
+     * @var string $username
+     */
+    protected $username;
+    
+    /**
+     * Property stores user password
+     * @var string $password
+     */
+    protected $password;
+
+    /**
+     * Property stores user email
+     * @var string $email
+     */
+    protected $email;
+
+    /**
+     * The Constructor method. Could set property values, or keep them null.
+     * @return void
+     */
+    public function __construct($id = null, $username = null, $password = null, $email = null)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
+
+    /**
+     * The setId method
+     * @return datatype description
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * The setId method
+     * @return void description
+     * @param datatype $id description
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * The getUsername method
+     * @return datatype description
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    
+    /**
+     * The setUsername method
+     * @return void description
+     * @param datatype $username description
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * The getPassword method
+     * @return datatype description
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    /**
+     * The setPassword method
+     * @return void description
+     * @param datatype $password description
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * The getEmail method
+     * @return datatype description
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+    /**
+     * The setEmail method
+     * @return void description
+     * @param datatype $email description
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
---
+++ src/Util/CPFValidator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by Thiago Mallon
+ */
+
+/**
+ * @package App\Lib\Util
+ */
+namespace App\Lib\Util;
+
+/**
+ * Trait CPFValidator
+ * @author Thiago Mallon <thiagomallon@gmail.com>
+ */
+trait CPFValidator
+{
+    
+}
